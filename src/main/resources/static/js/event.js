d053efec8cb49574cdb9fb4c214fb535d6423711
denisgrebenuk <dencheg995@gmail.com>
2019-03-28 02:12:12 +0300

    lk

--- src/main/resources/static/js/event.js
+++ src/main/resources/static/js/event.js
@@ -1,3 +1,20 @@
 function buyTicket(ticketPrice) {
 
+    var url = "https://money.yandex.ru/transfer?receiver=410018098845268&sum="+ ticketPrice
+    window.open(url);
+    // $.ajax({
+    //     type: "POST",
+    //     contentType: "application/json",
+    //     url: url,
+    //     success: function () {
+    //         location.reload();
+    //         console.log("Success");
+    //     },
+    //     error: function (e) {
+    //         alert("Неверный формат данных!");
+    //         console.log("ERROR: ", e);
+    //     }
+    // });
+
+    // https://money.yandex.ru/transfer?receiver=410018098845268-------sum=100-------successURL=-------quickpay-back-url=-------shop-host=-------label=-------targets=-------comment=-------origin=form-------selectedPaymentType=AC-------destination=-------form-comment=------short-dest=-------quickpay-form=donate
 }
