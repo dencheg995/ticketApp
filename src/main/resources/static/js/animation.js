c1ef120a37887a4c374e23a5d457260a9dcc153e
denisgrebenuk <dencheg995@gmail.com>
2019-04-15 22:11:29 +0300

    lk

--- src/main/resources/static/js/animation.js
+++ src/main/resources/static/js/animation.js
@@ -113,3 +113,20 @@ function textAreaAdjust(o) {
     o.style.width = "285px";
 
 }
+
+function showNotification(from, align) {
+    color = Math.floor((Math.random() * 4) + 1);
+
+    $.notify({
+        icon: "tim-icons icon-bell-55",
+        message: "Неверные логин или пароль"
+
+    }, {
+        type: type[color],
+        timer: 8000,
+        placement: {
+            from: from,
+            align: align
+        }
+    });
+}
