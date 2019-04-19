099dea55d6a40befb8ea0fa5ec3f9975dc3e7c43
agerus <alexander.gerus@grse.de>
2019-04-19 10:39:48 +0300

    css

--- src/main/resources/static/js/sale.js
+++ src/main/resources/static/js/sale.js
@@ -1,22 +1,5 @@
-function getSum(ticketId, ticketPrice) {
-    var count = $('span[value]');
-    var actionType = $('button[name]').char;
-    if (actionType === "up"){
-        count++;
-    }else if (actionType === "down" && count > 0){
-        count--;
-    }
-    var up = document.getElementById("up");
-    var logF = document.getElementById('values');
-    up.addEventListener("click", updateValueF);
+function addition() {
+    var currentCount = parseInt(document.getElementById("currentCount").value);
+    document.getElementById("sumResult").innerHTML = currentCount;
 
-    function updateValueF() {
-        logF.textContent = count;
-    }
-
-    var logS = document.getElementById('sum');
-    function updateValueS() {
-        logS.textContent = count;
-    }
-    up.addEventListener("click", updateValueS);
 };
