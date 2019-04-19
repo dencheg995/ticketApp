0af1ceb607ed6f90b9180b81803e1d635b939755
agerus <alexander.gerus@grse.de>
2019-04-20 01:07:56 +0300

    css

--- src/main/resources/static/js/sale.js
+++ src/main/resources/static/js/sale.js
@@ -1,44 +1,86 @@
 var sum = 0;
 var currentCount = 0;
-var prevCount = 0;
+var prevId = 0;
 localStorage.setItem("currentCount", currentCount);
-localStorage.setItem("prevCount", prevCount);
 localStorage.setItem("sum", sum);
-function addition(ticketId, ticketPrice) {
+function addition(ticketId, ticketPrice, ticketCountMax) {
     var cId = "currentCount" + ticketId;
-    // if (parseInt(localStorage.getItem("prevCount")) != parseInt(document.getElementById(cId).value)) {
+    $("#down" + ticketId).attr("disabled", false);
+    $("#down" + ticketId).attr("field", "off");
+    if (ticketCountMax != parseInt(document.getElementById(cId).value)) {
         currentCount = parseInt(localStorage.getItem("currentCount"));
         currentCount =+ parseInt(document.getElementById(cId).value);
         localStorage.setItem("currentCount", currentCount);
-        // localStorage.setItem("prevCount", currentCount);
-        sum = localStorage.getItem("sum");
-        sum =+ currentCount * ticketPrice;
+        if (parseInt(currentCount) == 0) {
+            $("#sumResult").val(sum);
+        }
+        sum = parseInt(localStorage.getItem("sum"));
+        sum += ticketPrice;
+
         localStorage.setItem("sum", sum);
-        // document.getElementById("sumResult").innerHTML = sum + " руб.";
+
+        localStorage.setItem("prevId", ticketId);
+
         $("#sumResult").val(sum);
-    // }
+    } else {
+        if ($("#up" + ticketId).attr("field") != "on") {
+            currentCount = parseInt(localStorage.getItem("currentCount"));
+            currentCount = +parseInt(document.getElementById(cId).value);
+            localStorage.setItem("currentCount", currentCount);
+            if (parseInt(currentCount) == 0) {
+                $("#sumResult").val(sum);
+            }
+            sum = parseInt(localStorage.getItem("sum"));
 
-    // var buttons = document.getElementsByTagName("button");
-    // var buttonsCount = buttons.length;
-    // for (var i = 0; i <= buttonsCount; i += 1) {
-    //     if (this.id == "down") {}
-    //     buttons[i].onclick = function(e) {
-    //         alert(this.id);
-    //     };
-    // }​
+            sum += ticketPrice;
+
+            localStorage.setItem("sum", sum);
+            if (parseInt(localStorage.getItem("prevId")) != parseInt(ticketId))
+                localStorage.setItem("prevId", ticketId);
+            $("#sumResult").val(sum);
+        }
+        $("#up" + ticketId).attr("field", "on");
+        $("#up" + ticketId).attr("disabled", true)
+    }
 }
 
-function subtraction(ticketId, ticketPrice) {
+function subtraction(ticketId, ticketPrice, ticketCountMax) {
     var cId = "currentCount" + ticketId;
-    currentCount = parseInt(localStorage.getItem("currentCount"));
-    currentCount =+ parseInt(document.getElementById(cId).value);
-    localStorage.setItem("currentCount", currentCount);
-    // localStorage.setItem("prevCount", currentCount);
-    sum = localStorage.getItem("sum");
-    sum =+ currentCount * ticketPrice;
-    localStorage.setItem("sum", sum);
-    // document.getElementById("sumResult").innerHTML = sum + " руб.";
-    $("#sumResult").val(sum);
-    //document.getElementById("sumResult").innerHTML = sum + " руб.";
-   // $("#sumResult").val(sum);
-}
+    $("#up" + ticketId).attr("disabled", false);
+    $("#up" + ticketId).attr("field", "off");
+    if (parseInt(document.getElementById(cId).value) > 0) {
+        currentCount = parseInt(localStorage.getItem("currentCount"));
+        currentCount = +parseInt(document.getElementById(cId).value);
+        if (parseInt(currentCount) == 0) {
+            $("#sumResult").val(sum);
+        }
+        localStorage.setItem("currentCount", currentCount);
+
+        sum = parseInt(localStorage.getItem("sum"));
+        sum -= ticketPrice;
+        localStorage.setItem("sum", sum);
+        localStorage.setItem("prevId", ticketId);
+        $("#sumResult").val(sum);
+    }
+         else {
+            if ($("#down" + ticketId).attr("field") != "on") {
+                currentCount = parseInt(localStorage.getItem("currentCount"));
+                currentCount = +parseInt(document.getElementById(cId).value);
+                localStorage.setItem("currentCount", currentCount);
+                if (parseInt(currentCount) == 0) {
+                    $("#sumResult").val(sum);
+                }
+                sum = parseInt(localStorage.getItem("sum"));
+
+                sum -= ticketPrice;
+                localStorage.setItem("sum", sum);
+                if (parseInt(localStorage.getItem("prevId")) != parseInt(ticketId))
+                    localStorage.setItem("prevId", ticketId);
+                $("#sumResult").val(sum);
+            }
+            $("#down" + ticketId).attr("disabled", true);
+            $("#down" + ticketId).attr("field", "on")
+
+    }
+    }
+
