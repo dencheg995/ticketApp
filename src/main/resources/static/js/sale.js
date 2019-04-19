d7ea412c69b68918dae333c409938e68ebf898c8
agerus <alexander.gerus@grse.de>
2019-04-19 18:05:50 +0300

    css

--- src/main/resources/static/js/sale.js
+++ src/main/resources/static/js/sale.js
@@ -1,4 +1,12 @@
-function addition() {
-    let currentCount = parseInt(document.getElementById("currentCount").value);
-    document.getElementById("sumResult").innerHTML = currentCount + " P";
+function addition(ticketId, ticketPrice) {
+    var cId = "currentCount" + ticketId;
+    let currentCount = parseInt(document.getElementById(cId).value);
+    var sum =+ currentCount*ticketPrice;
+    document.getElementById("sumResult").innerHTML = sum + " руб.";
+}
+function subtraction(ticketId, ticketPrice) {
+    var cId = "currentCount" + ticketId;
+    let currentCount = parseInt(document.getElementById(cId).value);
+    var sum =+ currentCount*ticketPrice;
+    document.getElementById("sumResult").innerHTML = sum + " руб.";
 }
